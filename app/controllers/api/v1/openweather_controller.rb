e37981d728171ee9801042b48c1fa0adc6a1d1ee
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-08-31 07:42:51 -0500

    Added client side ip

--- app/controllers/api/v1/openweather_controller.rb
+++ app/controllers/api/v1/openweather_controller.rb
@@ -1,15 +1,24 @@
-class Api::V1::OpenweatherController < ApplicationController
-  def index
-    uri_geo = URI("https://ipinfo.io?token=#{ENV["geo_token_api"]}")
-    res_geo = Net::HTTP.get_response(uri_geo)
-    output_geo = JSON.parse(res_geo.body)
-    coordinates = output_geo["loc"].split(",")
-    latitude = coordinates[0]
-    longitude = coordinates[1]
+# frozen_string_literal: true
 
-    uri_open_weather = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{latitude}&lon=#{longitude}&units=metric&appid=#{ENV["open_weather_api"]}")
-    res_open_weather = Net::HTTP.get_response(uri_open_weather)
-    output_open_weather = JSON.parse(res_open_weather.body)
-    render json: output_open_weather
+module Api
+  module V1
+    class OpenweatherController < ApplicationController
+      def index
+        ip = request.remote_ip
+        ip = '161.185.160.93' if ip.nil? || (ip == '127.0.0.1')
+        uri_geo = URI("https://ipinfo.io/#{ip}?token=#{ENV['geo_token_api']}")
+        puts uri_geo
+        res_geo = Net::HTTP.get_response(uri_geo)
+        output_geo = JSON.parse(res_geo.body)
+        coordinates = output_geo['loc'].split(',')
+        latitude = coordinates[0]
+        longitude = coordinates[1]
+
+        uri_open_weather = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{latitude}&lon=#{longitude}&units=metric&appid=#{ENV['open_weather_api']}")
+        res_open_weather = Net::HTTP.get_response(uri_open_weather)
+        output_open_weather = JSON.parse(res_open_weather.body)
+        render json: output_open_weather
+      end
+    end
   end
 end
