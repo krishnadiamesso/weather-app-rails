06f15f05d91d00b1658ade413a57d58d45c32e51
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-08-31 07:18:12 -0500

    Added request object

--- app/controllers/api/v1/opengeo_controller.rb
+++ app/controllers/api/v1/opengeo_controller.rb
@@ -10,9 +10,14 @@ class Api::V1::OpengeoController < ApplicationController
     uri_geo = URI("https://api.ipregistry.co/user_agent?key=tpgv21vzj2hjhlru")
     res_geo = Net::HTTP.get_response(uri_geo)
     output_geo = JSON.parse(res_geo.body)
-    render json: output_geo
+    req = {
+      "remote_ip": request.remote_ip,
+      "x_forwarded_for": request.env["HTTP_X_FORWARDED_FOR"],
+      "remote_addr": request.remote_addr,
+      "remote_address": request.env['REMOTE_ADDR'],
+    }
+    render json: req
   end
-
 
   def origin
     uri_geo = URI("https://api.ipregistry.co/?key=tpgv21vzj2hjhlru")
